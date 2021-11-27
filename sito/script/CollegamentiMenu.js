d2d68c38ec7166457ae5db5395851509af320746
GerosaSimone <s.gerosa0@gmail.com>
2021-11-27 11:32:05 +0100

    Sito html
    menu

---
+++ sito/script/CollegamentiMenu.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {
+    $.post("pagine/home.html", true, function(data, status) {
+        $("#pagina").html(data);
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#home", function() {
+        $.post("pagine/home.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#news", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#eventi", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#campionati", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#societa", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#albo", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#organigramma", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#storia", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#squadre", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#prima", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#junores", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#allievi", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#giovanissimi", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#esordienti", function() {
+        $.post("p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#pulcini", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#piccoli", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#stadio", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#come", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#informazioni", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#galleria", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
+
+$(document).ready(function() {
+    $(document).on('click', "#sponsor", function() {
+        $.post("pagine/p2.html", true, function(data, status) {
+            $("#pagina").html(data);
+        });
+    });
+})
