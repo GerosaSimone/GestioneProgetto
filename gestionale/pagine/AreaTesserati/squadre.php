6e7aa190b0ec2950ed3dce306786c1ae542d71e1
Payd3r <payd3r2003@gmail.com>
2022-04-10 19:49:51 +0200

    e

--- gestionale/pagine/AreaTesserati/squadre.php
+++ gestionale/pagine/AreaTesserati/squadre.php
@@ -8,6 +8,9 @@ if (!isset($_SESSION['user_id'])) {
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="table.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <style>
         .contenitore {
             width: auto;
@@ -17,163 +20,134 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 
 <body>
-
-</html>
-<div class="page-header clearfix">
-    <h2 class="pull-left" style="margin-left:30px"> <?php echo "   Giocatori "  . $_GET['squadra'] ?> </h2>
-</div>
-<div class="contenitore">
-    <div class="row">
-        <div class="col-sm-9">
-            <div class="page-header clearfix">
-                <h2 class="pull-left" style="margin-left:30px; margin-top:20px"> Mister </h2>
-            </div>
-            <div class="limiter">
-                <div class="container-table100">
-                    <div class="wrap-table100">
-                        <div class="table100">
-                            <?php
-                            require_once '../config.php';
-                            $sql = "SELECT * FROM categoria WHERE categoria.nome='" . $_GET['squadra'] . "'";
-                            $idCategoria = 0;
-                            $idVisita = 0;
-                            if ($result = mysqli_query($link, $sql)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    $row = mysqli_fetch_array($result);
-                                    $idCategoria = $row['id'];
-                                }
-                            }
-                            $sql = "SELECT * FROM tesserato WHERE idCategoria='" . $idCategoria . "'and tipo='1'";
-                            echo "<table id='modalDirigenza'><thead><tr class='table100-head'>";
-                            echo "      <th class='column1'>Nome</th>";
-                            echo "     <th class='column1'>Cognome</th>";
-                            echo "      <th class='column1'>Data Nascita</th>";
-                            echo "</tr></thead><tbody>";
-                            if ($result = mysqli_query($link, $sql)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        echo "<tr>";
-                                        echo "<td class='column1'>" . $row['nome'] . "</td>";
-                                        echo "<td class='column2'>" . $row['cognome'] . "</td>";
-                                        echo "<td class='column3'>" . $row['dataNascita'] . "</td>";
+    <div class="page-header clearfix">
+        <h2 class="pull-left" style="margin-left:30px"> <?php echo "   Giocatori "  . $_GET['squadra'] ?> </h2>
+    </div>
+    <div class="contenitore">
+        <div class="row">
+            <div class="col-sm-9">
+                <div class="page-header clearfix">
+                    <h2 class="pull-left" style="margin-left:30px; margin-top:20px"> Mister </h2>
+                </div>
+                <div class="limiter">
+                    <div class="container-table100">
+                        <div class="wrap-table100">
+                            <div class="table100">
+                                <?php
+                                require_once '../config.php';
+                                $sql = "SELECT * FROM categoria WHERE categoria.nome='" . $_GET['squadra'] . "'";
+                                $idCategoria = 0;
+                                $idVisita = 0;
+                                if ($result = mysqli_query($link, $sql)) {
+                                    if (mysqli_num_rows($result) > 0) {
+                                        $row = mysqli_fetch_array($result);
+                                        $idCategoria = $row['id'];
                                     }
-                                    mysqli_free_result($result);
-                                } else {
                                 }
-                            } else {
-                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                            }
-                            echo "</table>";
+                                $sql = "SELECT * FROM tesserato WHERE idCategoria='" . $idCategoria . "'and tipo='1'";
+                                echo "<table id='modalDirigenza'><thead><tr class='table100-head'>";
+                                echo "      <th class='column1'>Nome</th>";
+                                echo "     <th class='column1'>Cognome</th>";
+                                echo "      <th class='column1'>Data Nascita</th>";
+                                echo "</tr></thead><tbody>";
+                                if ($result = mysqli_query($link, $sql)) {
+                                    if (mysqli_num_rows($result) > 0) {
+                                        while ($row = mysqli_fetch_array($result)) {
+                                            echo "<tr>";
+                                            echo "<td class='column1'>" . $row['nome'] . "</td>";
+                                            echo "<td class='column2'>" . $row['cognome'] . "</td>";
+                                            echo "<td class='column3'>" . $row['dataNascita'] . "</td>";
+                                        }
+                                        mysqli_free_result($result);
+                                    } else {
+                                    }
+                                } else {
+                                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                                }
+                                echo "</table>";
 
-                            ?>
+                                ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="page-header clearfix">
-                <h2 class="pull-left" style="margin-left:30px; margin-top:20px"> Giocatori </h2>
-            </div>
-            <div class="limiter">
-                <div class="container-table100">
-                    <div class="wrap-table100">
-                        <div class="table100">
-                            <?php
-                            require_once '../config.php';
-                            $sql = "SELECT * FROM categoria WHERE categoria.nome='" . $_GET['squadra'] . "'";
-                            $idCategoria = 0;
-                            $idVisita = 0;
-                            if ($result = mysqli_query($link, $sql)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    $row = mysqli_fetch_array($result);
-                                    $idCategoria = $row['id'];
+                <div class="page-header clearfix">
+                    <h2 class="pull-left" style="margin-left:30px; margin-top:20px"> Giocatori </h2>
+                </div>
+                <div class="limiter">
+                    <div class="container-table100">
+                        <div class="wrap-table100">
+                            <div class="table100">
+                                <?php
+                                require_once '../config.php';
+                                $sql = "SELECT * FROM categoria WHERE categoria.nome='" . $_GET['squadra'] . "'";
+                                $idCategoria = 0;
+                                $idVisita = 0;
+                                if ($result = mysqli_query($link, $sql)) {
+                                    if (mysqli_num_rows($result) > 0) {
+                                        $row = mysqli_fetch_array($result);
+                                        $idCategoria = $row['id'];
+                                    }
                                 }
-                            }
-                            $sql = "SELECT * FROM tesserato WHERE idCategoria='" . $idCategoria . "'and tipo='0'";
-                            echo "<table id='modalGiocatori'><thead><tr class='table100-head'>";
-                            echo "      <th class='column1'>Nome</th>";
-                            echo "     <th class='column1'>Cognome</th>";
-                            echo "      <th class='column1'>Data Nascita</th>";
-                            echo "      <th class='column1'>Visita</th>";
-                            echo "      <th class='column1'>Scadenza</th>";
-                            echo "</tr></thead><tbody>";
-                            if ($result = mysqli_query($link, $sql)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        echo "<tr>";
-                                        echo "<td class='column1'>" . $row['nome'] . "</td>";
-                                        echo "<td class='column2'>" . $row['cognome'] . "</td>";
-                                        echo "<td class='column3'>" . $row['dataNascita'] . "</td>";
+                                $sql = "SELECT * FROM tesserato WHERE idCategoria='" . $idCategoria . "'and tipo='0'";
+                                echo "<table id='myTable'><thead><tr class='table100-head'>";
+                                echo "      <th class='column1'>Nome</th>";
+                                echo "      <th class='column1'>Cognome</th>";
+                                echo "      <th class='column1'>Data Nascita</th>";
+                                echo "      <th class='column1'>Visita</th>";
+                                echo "      <th class='column1'>Scadenza</th>";
+                                echo "</tr></thead><tbody>";
+                                if ($result = mysqli_query($link, $sql)) {
+                                    if (mysqli_num_rows($result) > 0) {
+                                        while ($row = mysqli_fetch_array($result)) {
+                                            echo "<tr>";
+                                            echo "<td class='column1'>" . $row['nome'] . "</td>";
+                                            echo "<td class='column2'>" . $row['cognome'] . "</td>";
+                                            echo "<td class='column3'>" . $row['dataNascita'] . "</td>";
 
-                                        $sql1 = "SELECT * FROM visita WHERE visita.id='" . $row['idVisita'] . "'";
-                                        if ($result2 = mysqli_query($link, $sql1)) {
-                                            if (mysqli_num_rows($result2) > 0) {
-                                                $row2 = mysqli_fetch_array($result2);
-                                                if (true)
-                                                    echo "<td class='column4'>  <span class='dot-green'></span>  </td>";
-                                                else
-                                                    echo "<td class='column4'>  <span class='dot-red'></span>  </td>";
-                                                echo "<td class='column5'>" . $row2['scadenza'] . "</td>";
-                                                echo "</tbody>";
+                                            $sql1 = "SELECT * FROM visita WHERE visita.id='" . $row['idVisita'] . "'";
+                                            if ($result2 = mysqli_query($link, $sql1)) {
+                                                if (mysqli_num_rows($result2) > 0) {
+                                                    $row2 = mysqli_fetch_array($result2);
+                                                    if (true)
+                                                        echo "<td class='column4'>  <span class='dot-green'></span>  </td>";
+                                                    else
+                                                        echo "<td class='column4'>  <span class='dot-red'></span>  </td>";
+                                                    echo "<td class='column5'>" . $row2['scadenza'] . "</td>";
+                                                    echo "</tbody>";
+                                                }
                                             }
                                         }
+                                        mysqli_free_result($result);
+                                    } else {
                                     }
-                                    mysqli_free_result($result);
                                 } else {
+                                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
                                 }
-                            } else {
-                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                            }
-                            echo "</table>";
+                                echo "</table>";
 
-                            mysqli_close($link);
-                            ?>
+                                mysqli_close($link);
+                                ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-3">
-            Prova prova prova
-        </div>
-    </div>
-</div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">User Details</h4>
-            </div>
-            <div class="modal-body">
-                <div class="username">
-                    <p>Name: </p><span></span>
-                </div>
-                <div class="position">
-                    <p>Position: </p><span></span>
-                </div>
-                <div class="office">
-                    <p>Office: </p><span></span>
-                </div>
-                <div class="age">
-                    <p>Age: </p><span></span>
-                </div>
-                <div class="date">
-                    <p>Start date: </p><span></span>
-                </div>
-                <div class="salary">
-                    <p>Salary: </p><span></span>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            <div class="col-sm-3">
+                Prova prova prova
             </div>
         </div>
     </div>
-</div>
 
 
-
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
 </body>
+
+</html>
---
+++ tabella/prova.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="table.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="limiter">
+        <div class="container-table100">
+            <div class="wrap-table100">
+                <div class="table100">
+                    <table id="example" class="display shadow-lg" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td id="name">Tiger Nixon</td>
+                                <td id="marks">System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>
+                                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#modifica">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                        </svg>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#elimina">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="name"> Garrett Winters</td>
+                                <td id="marks">Accountant</td>
+                                <td>Tokyo</td>
+                                <td>63</td>
+                                <td>2011/07/25</td>
+                                <td>
+                                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#modifica">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                        </svg>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#elimina">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                    </table>
+
+                    <!-- Elimina-->
+                    <div class="modal fade" id="elimina" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="eliminaLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="eliminaLabel">Elimina Giocatore</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modifica-->
+                    <div class="modal fade" id="modifica" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="modificaLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modificaLabel">Modifica Giocatore</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                paging: false,
+                searching: false,
+                ordering: true,
+                info: false
+            });
+        });
+        $("#modifica").click(function() {
+            var name = $("#name").val();
+            var marks = $("#marks").val();
+            var str = "You Have Entered " +
+                "Name: " + name +
+                " and Marks: " + marks;
+            $(".modal_body").html(str);
+        });
+    </script>
+</body>
+
+</html>
