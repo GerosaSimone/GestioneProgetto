4c7e3f9b2721b14f08b6de813d575c4e0be24dbb
GerosaSimone <s.gerosa0@gmail.com>
2022-10-16 08:49:39 +0200

    ok

--- sito/pagine/galleria.php
+++ sito/pagine/galleria.php
@@ -24,18 +24,25 @@
     </div>
     <div id="contenuto" class="container py-4">
         <div class="row">
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.rainews.it/dl/img/2022/01/06/1641502431627_GettyImages.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.rainews.it/dl/img/2022/01/06/1641502431627_GettyImages.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://images.agi.it/pictures/agi/agi/2020/03/09/161058470-ba123e71-21da-4865-989c-355c60128006.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://images.agi.it/pictures/agi/agi/2020/03/09/161058470-ba123e71-21da-4865-989c-355c60128006.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://cdn.corrieredellosport.it/images/2022/03/16/231325521-ae820c9d-9c5d-4fe6-b6d3-310729bdbac3.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://cdn.corrieredellosport.it/images/2022/03/16/231325521-ae820c9d-9c5d-4fe6-b6d3-310729bdbac3.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.centrosportivosancarlo.com/wp-content/uploads/sites/2/2019/03/pallone-calcio_1.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.centrosportivosancarlo.com/wp-content/uploads/sites/2/2019/03/pallone-calcio_1.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.calcioefinanza.it/wp-content/uploads/2022/04/Sportswear-adidas.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.calcioefinanza.it/wp-content/uploads/2022/04/Sportswear-adidas.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://focusicilia.it/wp-content/uploads/2021/10/Sport.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://focusicilia.it/wp-content/uploads/2021/10/Sport.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.my-personaltrainer.it/2020/07/10/gioco-del-calcio-nei-bambini-obbiettivi-orig.jpeg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.my-personaltrainer.it/2020/07/10/gioco-del-calcio-nei-bambini-obbiettivi-orig.jpeg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://images2.gazzettaobjects.it/methode_image/2022/08/02/Calcio/Foto_Calcio_-_Trattate/1410451690-krSG--800x450@Gazzetta-Web_800x450.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://images2.gazzettaobjects.it/methode_image/2022/08/02/Calcio/Foto_Calcio_-_Trattate/1410451690-krSG--800x450@Gazzetta-Web_800x450.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.milanoweekend.it/wp-content/uploads/2021/10/calcio-azione-gol-620x340.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.milanoweekend.it/wp-content/uploads/2021/10/calcio-azione-gol-620x340.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.rainews.it/dl/img/2022/06/24/1656099305732_francevitalyuefaeuropeanunderchampionshipgroupa.jpg" data-lightbox="photos"><img class="img-fluid temp  foto" src="https://www.rainews.it/dl/img/2022/06/24/1656099305732_francevitalyuefaeuropeanunderchampionshipgroupa.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.lisbona.info/wp-content/uploads/sites/21/calcio-lisbona-thumbnail.jpg" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.lisbona.info/wp-content/uploads/sites/21/calcio-lisbona-thumbnail.jpg"></a></div>
-            <div class="col-lg-3 col-sm-6 item"><a href="https://www.filodiritto.com/sites/default/files/styles/lg/public/2019-04/calcio.jpg?itok=1dC6KeT3" data-lightbox="photos"><img class="img-fluid temp foto" src="https://www.filodiritto.com/sites/default/files/styles/lg/public/2019-04/calcio.jpg?itok=1dC6KeT3"></a></div>
+
+            <?php
+            $sql = "    SELECT *
+                            FROM galleria
+                            ORDER BY id DESC";
+
+            if ($result = mysqli_query($link, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo '<div class="col-lg-3 col-sm-6 item"><a href="../../gestionale/img/uploadsGalleria/'.$row['foto'].'" data-lightbox="photos"><img class="img-fluid temp foto" src="../../gestionale/img/uploadsGalleria/'.$row['foto'].'"></a></div>';
+                    }
+                    mysqli_free_result($result);
+                }
+            } else {
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+            }
+
+            ?>
+
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js" integrity="sha512-k2GFCTbp9rQU412BStrcD/rlwv1PYec9SNrkbQlo6RZCf75l6KcC3UwDY8H5n5hl4v77IDtIPwOk9Dqjs/mMBQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
